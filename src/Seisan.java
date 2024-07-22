949c1d8519b886ff7b788a00ec134587b7a38d2c
shomakobayashi <shoma.kobayashi.slab@gmail.com>
2024-07-22 23:14:05 +0900

    Dレベル

---
+++ src/Seisan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Seisan {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+        int total = ( n * h );
+        System.out.println(total);
+    }
+
+}
